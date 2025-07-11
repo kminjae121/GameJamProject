c52b3fe0086f0abb88288245a2d6f67692cba1b8
김민재 <kbckmj2050@gmail.com>
2025-07-11 22:31:27 +0900

    feat : BasicSettings

---
+++ Assets/Member/00.Common/01.Scripts/01.Input/InputReader.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[CreateAssetMenu(fileName = "PlayerInput", menuName = "SO/PlayerInput", order = 0)]
+public class InputReader : ScriptableObject, Controls.IPlayerActions
+{
+    public Vector2 MovementKey { get; private set; }
+    
+    private Controls _controls;
+    
+    public event Action OnJumpEvent;
+
+    public event Action OnMoveEvent;
+    
+    public event Action OnAttackEvent;
+    private void OnEnable()
+    {
+        if (_controls == null)
+        {
+            _controls = new Controls();
+            _controls.Player.SetCallbacks(this);
+        }
+
+        _controls.Player.Enable();
+    }
+    
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        MovementKey = context.ReadValue<Vector2>();
+
+        if (context.performed)
+            OnMoveEvent?.Invoke();
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+            OnAttackEvent?.Invoke();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+            OnJumpEvent?.Invoke();
+    }
+}
