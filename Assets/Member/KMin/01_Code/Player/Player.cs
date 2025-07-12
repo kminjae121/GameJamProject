4d645feba44e37a436918fd0957156dd72cd3b90
dlckdgh0414 <ci5976547@gmail.com>
2025-07-12 11:52:39 +0900

    fix : PlayerError

--- Assets/Member/KMin/01_Code/Player/Player.cs
+++ Assets/Member/KMin/01_Code/Player/Player.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class Player : Entity
 {
-    
+    public override void OnDead()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnHit()
+    {
+        throw new System.NotImplementedException();
+    }
 }
