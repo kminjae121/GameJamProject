b3bf3a73be60b1acf7e3ed976ef25ecc2caeb59a
rlaals0203 <scientistmin@gmail.com>
2025-07-13 03:55:08 +0900

    feat : attack speed

--- Assets/Member/KMin/00_Scene/01_Code/Player/PlayerAttackCompo.cs
+++ Assets/Member/KMin/00_Scene/01_Code/Player/PlayerAttackCompo.cs
@@ -10,6 +10,7 @@ public class PlayerAttackCompo : MonoBehaviour, IEntityComponent, IAfterInitiali
     [SerializeField] private GameObject arrowObject;
     [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private Transform projectileParent;
+    [SerializeField] private float coolTime = 0.1f;
 
     [SerializeField] private StatSO atkDamage;
     [SerializeField] private StatSO atkcoolTime;
@@ -22,6 +23,9 @@ public class PlayerAttackCompo : MonoBehaviour, IEntityComponent, IAfterInitiali
     private Vector2 _direction;
     private float _angle;
 
+    private float _time;
+    private bool _canAttack;
+
     public void Initialize(Entity entity)
     {
         _statCompo = entity.GetCompo<EntityStat>();
@@ -31,6 +35,8 @@ public class PlayerAttackCompo : MonoBehaviour, IEntityComponent, IAfterInitiali
     {
         playerInput.OnPointerEvent += HandlePointer;
         playerInput.OnClickEvent += HandleClick;
+
+        _time = Time.time;
     }
     
     public void AfterInitialize()
@@ -42,6 +48,14 @@ public class PlayerAttackCompo : MonoBehaviour, IEntityComponent, IAfterInitiali
     {
         float changed = currentvalue - prevvalue;
         _atkDamage += changed;
+    }
+
+    private void Update()
+    {
+        if (Time.time - _time > coolTime)
+        {
+            _canAttack = true;
+        }
     }
 
     private void HandleClick() => SpawnProjectile();
@@ -65,7 +79,7 @@ public class PlayerAttackCompo : MonoBehaviour, IEntityComponent, IAfterInitiali
 
     private void SpawnProjectile()
     {
-        if (this == null) return;
+        if (this == null || _canAttack == false) return;
         Vector3 firePos = (Vector2)transform.position + _direction.normalized * 2f;
         if (shootCnt == 1)
         {
@@ -113,8 +127,10 @@ public class PlayerAttackCompo : MonoBehaviour, IEntityComponent, IAfterInitiali
                 }
                 else
                     projectile.InitProjectile(_angle);
-                
             }
         }
+        
+        _canAttack = false;
+        _time = Time.time;
     }
 }
