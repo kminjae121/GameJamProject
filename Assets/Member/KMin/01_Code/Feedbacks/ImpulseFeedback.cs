c0f0549a305f4b3f4629a6ff20421c438e619c5e
rlaals0203 <scientistmin@gmail.com>
2025-07-12 16:35:45 +0900

    feat : impulse feedback

---
+++ Assets/Member/KMin/01_Code/Feedbacks/ImpulseFeedback.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Cinemachine;
+using UnityEngine;
+
+namespace Code.Feedbacks
+{
+    public class ImpulseFeedback : Feedback
+    {
+        [SerializeField] private CinemachineImpulseSource _impulseSource;
+        [SerializeField] private float impulseForce;
+        
+        public override void CreateFeedback()
+        {
+            _impulseSource.GenerateImpulse(impulseForce);
+        }
+
+        public override void StopFeedback()
+        {
+        }
+    }
+}
