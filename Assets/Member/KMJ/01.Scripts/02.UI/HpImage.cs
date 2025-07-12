d2b634322bb824111dc3a574ff31802343ef477e
rlaals0203 <scientistmin@gmail.com>
2025-07-13 01:34:28 +0900

    feat : scene transition particle

--- Assets/Member/KMJ/01.Scripts/02.UI/HpImage.cs
+++ Assets/Member/KMJ/01.Scripts/02.UI/HpImage.cs
@@ -1,4 +1,5 @@
 using System;
+using Blade.Core;
 using Code.Combat;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,12 +7,16 @@ using UnityEngine.UI;
 public class HpImage : MonoBehaviour
 {
     [SerializeField] private Image _image;
-
     [SerializeField] private EntityHealth health;
 
+    private float _prevHp = 0f;
+    private void Awake()
+    {
+    }
 
     private void Update()
     {
-        _image.fillAmount = health.CurrentHealth / health.MaxHealth;
+        _image.fillAmount = Mathf.Lerp(_prevHp,  1 - health.CurrentHealth / health.MaxHealth, 1f);
+        _prevHp = 1 - health.CurrentHealth / health.MaxHealth;
     }
 }
---
+++ Assets/Member/KMin/00_Scene/01_Code/Feedbacks/PlayerDeadFeedback.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Member.KMin._00_Scene._01_Code.Feedbacks
+{
+    public class PlayerDeadFeedback : Feedback
+    {
+        public override void CreateFeedback()
+        {
+            
+        }
+
+        public override void StopFeedback()
+        {
+        }
+    }
+}
