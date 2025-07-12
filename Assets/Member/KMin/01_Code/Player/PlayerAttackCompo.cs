360667fb0dfcff8a45c83c48b1585993cd100c36
rlaals0203 <scientistmin@gmail.com>
2025-07-12 13:57:59 +0900

    fix : fire direction

--- Assets/Member/KMin/01_Code/Player/PlayerAttackCompo.cs
+++ Assets/Member/KMin/01_Code/Player/PlayerAttackCompo.cs
@@ -47,13 +47,13 @@ public class PlayerAttackCompo : MonoBehaviour, IEntityComponent
         RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, 100f, whatIsEnemy);
         Vector3 endPos = hit ? hit.point : transform.right * 50f;
         
-        _attackDirLine.SetPosition(0,  transform.position + _direction * 5f);
+        _attackDirLine.SetPosition(0,  transform.position + _direction.normalized * 2f);
         _attackDirLine.SetPosition(1, endPos);
     }
 
     private void SpawnProjectile()
     {
-        Projectile projectile = Instantiate(projectilePrefab, transform.position + _direction * 5f, 
+        Projectile projectile = Instantiate(projectilePrefab, transform.position + _direction.normalized * 2f, 
             quaternion.identity, projectileParent).GetComponent<Projectile>();
         
         projectile.InitProjectile(_angle);
