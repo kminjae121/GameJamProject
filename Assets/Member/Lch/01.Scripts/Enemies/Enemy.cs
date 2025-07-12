dc34bbd0d7928ec2a7f0890d1d7e6cb95eb7032e
dlckdgh0414 <ci5976547@gmail.com>
2025-07-12 12:11:37 +0900

    fix : EnemyAttack

--- Assets/Member/Lch/01.Scripts/Enemies/Enemy.cs
+++ Assets/Member/Lch/01.Scripts/Enemies/Enemy.cs
@@ -1,13 +1,17 @@
+using Code.Combat;
 using UnityEngine;
 
 public abstract class Enemy : Entity
 {
+    [SerializeField] private StatSO attackDamageStat;
     protected EnemyMovement movement;
+    private EntityStat _statCompo;
 
     protected override void Awake()
     {
         base.Awake();
         movement = GetCompo<EnemyMovement>();
+        _statCompo = GetCompo<EntityStat>();
     }
 
     public override void OnDead()
@@ -19,5 +23,22 @@ public abstract class Enemy : Entity
     public override void OnHit()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if(collision.gameObject.TryGetComponent(out IDamageable damageable))
+            {
+                DamageData damageData = new DamageData();
+                damageData.damage = _statCompo.GetStat(attackDamageStat).Value;
+                damageData.isCritical = false;
+                damageData.hitPoint = transform.position;
+                damageData.hitNormal = transform.position.normalized;
+                damageable.ApplyDamage(damageData, transform.position);
+                Destroy(gameObject);
+            }
+        }
     }
 }
